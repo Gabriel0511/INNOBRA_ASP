1e8a121acda828f67eca87cf6fc4d4e4210f7594
Stefano Gaggero <stefanolucagaggero@gmail.com>
2024-09-29 17:11:27 -0300

    AutoMapperProfiles

---
+++ INNOBRA_ASP.Server/Util/AutoMapperProfiles.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using INNOBRA_ASP.DB.Data.Entity;
+using INNOBRA.SHARED.DTO;
+
+namespace INNOBRA_ASP.Server.Util
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            //CreateMap<CrearObraDTO, Obra>();
+
+            //CreateMap<EditarObraDTO, Obra>();
+        }
+    }
+}
