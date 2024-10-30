c74162d7bdbe639e8358d6dfbef43f151dd738f7
Stefano Gaggero <stefanolucagaggero@gmail.com>
2024-10-29 22:20:30 -0300

    inyeccion de httpservicio y alerta servicio en program del client

--- INNOBRA_ASP.Client/Program.cs
+++ INNOBRA_ASP.Client/Program.cs
@@ -1,4 +1,5 @@
 using INNOBRA_ASP.Client;
+using INNOBRA_ASP.Client.Servicios;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +8,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped<IHttpServicio, HttpServicio>();
+builder.Services.AddScoped<AlertaServicio>();
 
 await builder.Build().RunAsync();
