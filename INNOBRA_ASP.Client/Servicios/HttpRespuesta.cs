8c8ef1f1d52a825c8e5c06c627cea688002cd211
Stefano Gaggero <stefanolucagaggero@gmail.com>
2024-10-29 22:14:10 -0300

    HttpServicio, interfaz y http respuesta, maqueta de alerta servicio para usar mas adelante

---
+++ INNOBRA_ASP.Client/Servicios/HttpRespuesta.cs
@@ -0,0 +1,43 @@
+﻿namespace INNOBRA_ASP.Client.Servicios
+{
+    public class HttpRespuesta<T>
+    {
+        public T Respuesta { get; }
+
+        public bool Error { get; }
+
+        public HttpResponseMessage HttpResponseMessage { get; set; }
+
+        public HttpRespuesta(T respuesta, bool error, HttpResponseMessage httpResponseMessage)
+        {
+            Respuesta = respuesta;
+            Error = error;
+            HttpResponseMessage = httpResponseMessage;
+        }
+
+        public async Task<string> ObtenerError()
+        {
+            if (!Error)
+            {
+                return "";
+            }
+
+            var statuscode = HttpResponseMessage.StatusCode;
+
+            switch (statuscode)
+            {
+                case System.Net.HttpStatusCode.BadRequest:
+                    return HttpResponseMessage.Content.ReadAsStringAsync().ToString()!;
+                //               return "Error, no se puede procesar la informacion";
+                case System.Net.HttpStatusCode.Unauthorized:
+                    return "Error, no esta logueado";
+                case System.Net.HttpStatusCode.Forbidden:
+                    return "Error, no tiene autorizacion a ejecutar este proceso";
+                case System.Net.HttpStatusCode.NotFound:
+                    return "Error, direccion no encontrada";
+                default:
+                    return HttpResponseMessage.Content.ReadAsStringAsync().Result;
+            }
+        }
+    }
+}
