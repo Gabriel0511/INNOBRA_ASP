2c20a6b943cea4d6d123a9cf8aa0647e17739383
Pablo Exequiel Gutierrez <pablo.e.gutierrez@outlook.com>
2024-11-05 02:24:03 -0300

    Crear

--- INNOBRA_ASP.DB/Migrations/20241105045756_correccion2.Designer.cs
+++ INNOBRA_ASP.DB/Migrations/20241105045756_correccion2.Designer.cs
@@ -4,6 +4,7 @@ using INNOBRA_ASP.DB.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace INNOBRA_ASP.DB.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20241105045756_correccion2")]
+    partial class correccion2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
