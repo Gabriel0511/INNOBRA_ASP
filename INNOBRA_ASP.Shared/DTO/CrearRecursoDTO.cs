999d2434c6a8bc3c25be8eace598a75bd368e0a9
Stefano Gaggero <stefanolucagaggero@gmail.com>
2025-02-23 17:23:59 -0300

    Prueba DTO

--- INNOBRA_ASP.Shared/DTO/CrearRecursoDTO.cs
+++ INNOBRA_ASP.Shared/DTO/CrearRecursoDTO.cs
@@ -19,11 +19,14 @@ namespace INNOBRA_ASP.Shared.DTO
 
         public TipoRecursoDTO Tipo { get; set; }
 
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
         public string Nombre { get; set; }
 
+        [Required(ErrorMessage = "La cantidad es obligatoria.")]
         public string Cantidad { get; set; }
 
         //CLAVE FORANEA
+
         public int Unidad_Id { get; set; }
 
     }
