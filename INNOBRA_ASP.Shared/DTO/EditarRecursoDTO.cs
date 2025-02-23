9280b412d63191d51a738777103cf65d1b714324
Stefano Gaggero <stefanolucagaggero@gmail.com>
2025-02-23 20:02:55 -0300

    Validaciones en Editar Recurso

--- INNOBRA_ASP.Shared/DTO/EditarRecursoDTO.cs
+++ INNOBRA_ASP.Shared/DTO/EditarRecursoDTO.cs
@@ -1,6 +1,7 @@
 ﻿using INNOBRA_ASP.DB.Data.Entity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,8 +21,12 @@ namespace INNOBRA_ASP.Shared.DTO
 
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        [RegularExpression(@"^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$", ErrorMessage = "Nombre invalido.")]
         public string Nombre { get; set; }
 
+        [Required(ErrorMessage = "La cantidad es obligatoria.")]
+        [Range(1, int.MaxValue, ErrorMessage = "La cantidad debe ser un numerica.")]
         public string Cantidad { get; set; }
 
         //CLAVE FORANEA
