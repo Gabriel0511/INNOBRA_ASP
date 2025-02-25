cb1f2d88b4398a6e2912073b20134f0856679f81
Stefano Gaggero <stefanolucagaggero@gmail.com>
2025-02-25 00:13:41 -0300

    Correcion en Editar Obra

--- INNOBRA_ASP.Shared/DTO/EditarObraDTO.cs
+++ INNOBRA_ASP.Shared/DTO/EditarObraDTO.cs
@@ -11,6 +11,7 @@ namespace INNOBRA_ASP.Shared.DTO
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
         public string Nombre { get; set; }
 
         [Required(ErrorMessage = "La fecha de inicio es obligatoria.")]
