818f5fcedab018527b2fea36be906aa1881c81d6
Stefano Gaggero <stefanolucagaggero@gmail.com>
2025-02-25 15:15:08 -0300

    Validaciones Crear Item

--- INNOBRA_ASP.Shared/DTO/CrearItemDTO.cs
+++ INNOBRA_ASP.Shared/DTO/CrearItemDTO.cs
@@ -11,16 +11,20 @@ namespace INNOBRA_ASP.Shared.DTO
     public class CrearItemDTO
     {
         [Required(ErrorMessage = "El tiempo estimado es obligatorio.")]
+        [Range(1, int.MaxValue, ErrorMessage = "El tiempo estimado debe ser mayor a 0")]
         public int Tiempo_estimado { get; set; }
 
         [Required(ErrorMessage = "La unidad de tiempo es obligatoria.")]
         public string Unidad_Tiempo { get; set; }
 
         [Required(ErrorMessage = "El material estimado es obligatorio.")]
+        [Range(1, int.MaxValue, ErrorMessage = "El material estimado debe ser mayor a 0")]
         public int Material_estimado { get; set; }
 
         // clave foranea
 
+        //[RegularExpression(@"^(?!-).*$", ErrorMessage = "Seleccione una opcion")]
+        [Range(1, int.MaxValue, ErrorMessage = "Seleccione un item tipo valido")]
         public int Item_Tipos_Id{ get; set; }
 
         public int Presupuesto_Id { get; set; }
