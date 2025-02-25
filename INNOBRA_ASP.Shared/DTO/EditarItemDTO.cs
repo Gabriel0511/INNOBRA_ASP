d315aa1d5cea51d89350d38f9cb062b8a13f50d1
Stefano Gaggero <stefanolucagaggero@gmail.com>
2025-02-25 15:30:53 -0300

    Validaciones Editar Item

--- INNOBRA_ASP.Shared/DTO/EditarItemDTO.cs
+++ INNOBRA_ASP.Shared/DTO/EditarItemDTO.cs
@@ -13,12 +13,14 @@ namespace INNOBRA_ASP.Shared.DTO
         public int Id { get; set; }
 
         [Required(ErrorMessage = "El tiempo estimado es obligatorio.")]
+        [Range(1, int.MaxValue, ErrorMessage = "El tiempo estimado debe ser mayor a 0")]
         public int Tiempo_estimado { get; set; }
 
         [Required(ErrorMessage = "La unidad de tiempo es obligatoria.")]
         public string Unidad_Tiempo { get; set; }
 
         [Required(ErrorMessage = "El material estimado es obligatorio.")]
+        [Range(1, int.MaxValue, ErrorMessage = "El material estimado debe ser mayor a 0")]
         public int Material_estimado { get; set; }
     }
 }
