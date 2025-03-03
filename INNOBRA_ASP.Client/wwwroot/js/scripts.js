54741dca8008b7594dae997b13ce10ae11a42fce
Gabriel Casas <gcasas132@gmail.com>
2025-03-03 20:40:05 -0300

    pdf

---
+++ INNOBRA_ASP.Client/wwwroot/js/scripts.js
@@ -0,0 +1,10 @@
+﻿function generarPDF(contenido) {
+    // Crear una instancia de jsPDF
+    const doc = new jspdf.jsPDF();
+
+    // Agregar el contenido al PDF
+    doc.text(contenido, 10, 10);
+
+    // Guardar el PDF con un nombre de archivo
+    doc.save("informe.pdf");
+}
